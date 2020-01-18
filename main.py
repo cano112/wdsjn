9b2d4fecdb32e88b46601f099a646e8fcb05cf94
Kamil Noster <kamil.noster@gmail.com>
2020-01-18 23:17:40 +0100

    Rewrite algorithms setup

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from embeddings.EmbeddingModelWrapper import EmbeddingModelWrapper
 from ensembled.CustomEnsembleSimilarity import CustomEnsembleSimilarity
 from jaccard.JaccardSimilarity import JaccardSimilarity
 from wmd.WordMoverDistance import WordMoverDistance
+from enum import Enum
 
 import argparse
 
 
+class Algorithm(Enum):
+    jaccard = 1
+    wmd = 2
+    cosine = 3
+    ensemble = 4
+    sif_cosine = 5
+
+
 def main():
-    default_algo = "jaccard"
     nkjp_wiki_path = "embeddings/nkjp+wiki-lemmas-all-100-cbow-hs.txt"
 
     parser = argparse.ArgumentParser()
     parser.add_argument("sentence1", help='Pierwsze zdanie do porównania.', type=str)
     parser.add_argument("sentence2", help='Drugie zdanie do porównania.', type=str)
-    parser.add_argument("-a", "--algorithm", help="Użyty algorytm (jeden z: {})".format(algos.keys()),
-                        type=str, default=default_algo)
+    parser.add_argument("-a", "--algorithm", help="Użyty algorytm (jeden z: {})".format([a.name for a in Algorithm]),
+                        type=str, default=Algorithm.jaccard.name)
     parser.add_argument("-e", "--embedding", help="Ścieżka do pliku z modelem word2vec.",
                         type=str, default=nkjp_wiki_path)
     args = parser.parse_args()
@@ -34,11 +42,11 @@ def main():
     ])
 
     algos = {
-        "jaccard": jaccard,
-        "wmd": wmd,
-        "cosine": cosine,
-        "ensemble": ensemble,
-        "sif-cosine": sif_cosine
+        Algorithm.jaccard.name: jaccard,
+        Algorithm.wmd.name: wmd,
+        Algorithm.cosine.name: cosine,
+        Algorithm.ensemble.name: ensemble,
+        Algorithm.sif_cosine: sif_cosine
     }
 
     result = algos[args.algorithm].normalized_score(args.sentence1, args.sentence2)
